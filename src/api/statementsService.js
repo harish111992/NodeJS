f75738473c4965c49f1cd676cda7de0775c5e5f6
harish111992 <63441336+harish111992@users.noreply.github.com>
2025-07-08 21:37:04 +0530

    Create statementsService.js

---
+++ src/api/statementsService.js
@@ -0,0 +1,6 @@
+export const getStatements = async () => {
+  return Promise.resolve([
+    { id: 1, description: "Monthly Statement - Jan" },
+    { id: 2, description: "Monthly Statement - Feb" }
+  ]);
+};
